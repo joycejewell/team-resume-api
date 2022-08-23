26ebe5e90a8789062ba132dab3a392f20fe3a3af
joycejewell <joyceroop@gmail.com>
2022-08-22 20:30:42 -0600

    added models / controllers / schema / seeds

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
-  has_many: experience
-  has_many: education
-  has_many: skills
-  has_many: capstone
+  has_many :experience
+  has_many :education
+  has_many :skills
+  has_many :capstone
 end
