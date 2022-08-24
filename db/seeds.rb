3e60ad73c91e00e64fe108811977d765fd735a71
Caroline Militello <carolinemilitello@gmail.com>
2022-08-23 18:36:04 -0700

    cruded everything but still getting errors for create and update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
-Student.create(first_name: "DJ Jazzy", last_name: "Jones", email: "djj@gmail.com", phone_number: "1234567890", short_bio: "I spin vinyl", linkedin_url: "", twitter_handle: "", personal_blog_url: "", online_resume_url: "", github_url: "", photo: "")
+Student.create!(first_name: "DJ Jazzy", last_name: "Jones", email: "djj@gmail.com", phone_number: "1234567890", short_bio: "I spin vinyl", linkedin_url: "a", twitter_handle: "a", personal_blog_url: "a", online_resume_url: "a", github_url: "a", photo: "a")
+
+Student.create!(first_name: "test 2", last_name: "test 2", email: "", phone_number: "", short_bio: "", linkedin_url: "", twitter_handle: "", personal_blog_url: "", online_resume_url: "", github_url: "", photo: "")
