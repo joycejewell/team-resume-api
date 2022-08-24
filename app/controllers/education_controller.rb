9a8d181f3b97f89f95d40bbf21220ab722b0d7bb
joycejewell <joyceroop@gmail.com>
2022-08-23 20:18:46 -0600

    education and experience

--- app/controllers/education_controller.rb
+++ app/controllers/education_controller.rb
@@ -1,2 +1,55 @@
 class EducationController < ApplicationController
+  def index
+    @educations = Education.all
+    render json: @educations.as_json
+  end
+
+  def show
+    @education = education.find_by(id: params[:id])
+    render json: @education.as_json
+  end
+
+  def create
+    education = Education.new(
+      student_id: params[:student_id],
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      degree: params[:degree],
+      university_name: params[:university_name],
+      details: params[:details],
+    )
+    # education.save
+    # render json: education.as_json
+    if education.save
+      render json: { message: "education created!" }, status: :created
+    else
+      render json: { errors: education.errors.full.messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    education_id = params[:id]
+    education = Education.find_by(id: education_id)
+    education.destroy
+    render json: {message: "Deleted education"}
+  end
+
+  def update
+    education_id = params["id"]
+    education = Education.find_by(id:education_id)
+    education.student_id = params[:student_id] || education.student_id
+    education.start_date = params[:start_date] || education.start_date
+    education.end_date = params[:end_date] || education.end_date
+    education.degree = params[:degree] || education.degree
+    education.university_name = params[:university_name] || education.university_name
+    education.details = params[:details] || education.details
+
+
+    if education.save
+      render json: education.as_json
+    else
+      render json: {errors: education.error.full_messages}, status: 422
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 Student.create!(first_name: "DJ Jazzy", last_name: "Jones", email: "djj@gmail.com", phone_number: "1234567890", short_bio: "I spin vinyl", linkedin_url: "a", twitter_handle: "a", personal_blog_url: "a", online_resume_url: "a", github_url: "a", photo: "a")
 
 Student.create!(first_name: "test 2", last_name: "test 2", email: "", phone_number: "", short_bio: "", linkedin_url: "", twitter_handle: "", personal_blog_url: "", online_resume_url: "", github_url: "", photo: "")
+
+
+Experience.create!(student_id: 2, start_date: "June 1", end_date: "May 30", company_name: "Vinyl Spins", details: "lets learn")
+
+
+Education.create!(student_id: 1, start_date: "June 1", end_date: "May 30", degree: "Music Data",university_name: "Vinyl University", details: "lets learn" )
