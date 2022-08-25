94d2341122737c7f46e04ab7effeb5c4442bae14
joycejewell <joyceroop@gmail.com>
2022-08-24 18:01:20 -0600

    added login, user, backend auth

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,24 @@ Rails.application.routes.draw do
   patch "skills/:id" => "skills#update"
   delete "skills/:id" => "skills#destroy"
   
+  #experience
+  get "/experience" => "experience#index"
+  get "/experience/:id" => "experience#show"
+  post "/experience" => "experience#create"
+  patch "experience/:id" => "experience#update"
+  delete "experience/:id" => "experience#destroy"
 
+  #education
+  get "/education" => "education#index"
+  get "/education/:id" => "education#show"
+  post "/education" => "education#create"
+  patch "education/:id" => "education#update"
+  delete "education/:id" => "education#destroy"
 
+  #users
+  post "/users" => "users#create"
+
+  #sessions (login)
+  post "/sessions" => "sessions#create"
 
 end
